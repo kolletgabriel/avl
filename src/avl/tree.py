708676019b4cc0ae984575dee9b8f1a0628ac30c
Gabriel Kollet <gabrielkollet99@gmail.com>
2025-07-14 19:32:31 -0300

    Adding traversal methods.

--- src/avl/tree.py
+++ src/avl/tree.py
@@ -44,6 +44,39 @@ class Tree:
             return True
         return self._recursive_insert(key, self.__root)
 
+    def _recursive_preorder(self, node: Node | None, keys: list[int]) -> None:
+        if node is None:
+            return None
+        keys.append(node.key)
+        self._recursive_preorder(node.lchild, keys)
+        self._recursive_preorder(node.rchild, keys)
+
+    def _recursive_inorder(self, node: Node | None, keys: list[int]) -> None:
+        if node is None:
+            return None
+        self._recursive_inorder(node.lchild, keys)
+        keys.append(node.key)
+        self._recursive_inorder(node.rchild, keys)
+
+    def _recursive_postorder(self, node: Node | None, keys: list[int]) -> None:
+        if node is None:
+            return None
+        self._recursive_postorder(node.lchild, keys)
+        self._recursive_postorder(node.rchild, keys)
+        keys.append(node.key)
+
+    def traverse(self, order:str) -> list[int]:
+        result: list[int] = []
+        if order == 'preorder':
+            self._recursive_preorder(self.__root, result)
+        elif order == 'inorder':
+            self._recursive_inorder(self.__root, result)
+        elif order == 'postorder':
+            self._recursive_postorder(self.__root, result)
+        else:
+            raise ValueError('Order must be: preorder, inorder or postorder.')
+        return result
+
 
 if __name__ == '__main__':
     ...
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -10,6 +10,7 @@ class TestTree(unittest.TestCase):
         del self.t
 
     def test_insert(self) -> None:
+        self.assertIsNone(self.t.root)
         self.t.insert(8)
         self.assertIsNotNone(self.t.root)
         self.t.insert(6)
@@ -27,6 +28,66 @@ class TestTree(unittest.TestCase):
         self.assertFalse(self.t.find(9))
         self.t.insert(9)
         self.assertTrue(self.t.find(9))
+
+    def test_preorder(self) -> None:
+        correct_order = [8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15]
+        t = Tree()
+        t.insert(8)
+        t.insert(4)
+        t.insert(12)
+        t.insert(2)
+        t.insert(6)
+        t.insert(10)
+        t.insert(14)
+        t.insert(1)
+        t.insert(3)
+        t.insert(5)
+        t.insert(7)
+        t.insert(9)
+        t.insert(11)
+        t.insert(13)
+        t.insert(15)
+        self.assertEqual(t.traverse('preorder'), correct_order)
+
+    def test_inorder(self) -> None:
+        t = Tree()
+        t.insert(9)
+        t.insert(7)
+        t.insert(8)
+        t.insert(6)
+        t.insert(5)
+        t.insert(1)
+        t.insert(3)
+        t.insert(2)
+        t.insert(4)
+        t.insert(10)
+        t.insert(16)
+        t.insert(15)
+        t.insert(14)
+        t.insert(13)
+        t.insert(12)
+        t.insert(11)
+        self.assertEqual(t.traverse('inorder'), list(range(1,17)))
+
+    def test_postorder(self) -> None:
+        correct_order = [1, 3, 2, 5, 7, 6, 4, 9, 11, 10, 13, 15, 14, 12, 8]
+        t = Tree()
+        t.insert(8)
+        t.insert(4)
+        t.insert(12)
+        t.insert(2)
+        t.insert(6)
+        t.insert(10)
+        t.insert(14)
+        t.insert(1)
+        t.insert(3)
+        t.insert(5)
+        t.insert(7)
+        t.insert(9)
+        t.insert(11)
+        t.insert(13)
+        t.insert(15)
+        self.assertEqual(t.traverse('postorder'), correct_order)
 
 
 if __name__ == '__main__':
