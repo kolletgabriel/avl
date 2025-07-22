aa9cb8041701f2094957d6b78149b9e06e8dec05
Gabriel Kollet <gabrielkollet99@gmail.com>
2025-07-22 19:28:11 -0300

    Rewriting node class; adding comparsion dunders.

--- src/avl/node.py
+++ src/avl/node.py
@@ -1,59 +1,46 @@
 class Node:
+
     def __init__(self, key: int,
                  parent: 'Node | None' = None,
                  lchild: 'Node | None' = None,
                  rchild: 'Node | None' = None) -> None:
         self.key = key
-        self.__parent = parent
-        self.__lchild = lchild
-        self.__rchild = rchild
+        self.parent = parent
+        self.lchild = lchild
+        self.rchild = rchild
 
     def __str__(self) -> str:
         return f'{self.key}'
 
     def __repr__(self) -> str:
-        return f'Node({self.key},{self.parent},{self.lchild},{self.rchild})'
+        return f'Node(key={self.key},\
+                parent={self.parent},\
+                lchild={self.lchild},rchild={self.rchild})'
 
-    @property
-    def parent(self) -> 'Node | None':
-        return self.__parent
+    def __eq__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.key == other
+        return self.key == other.key
 
-    @parent.setter
-    def parent(self, new_parent: 'Node | None') -> None:
-        self.__parent = new_parent
+    def __gt__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.key > other
+        return self.key > other.key
 
-    @property
-    def lchild(self) -> 'Node | None':
-        return self.__lchild
+    def __ge__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.key >= other
+        return self.key >= other.key
 
-    @lchild.setter
-    def lchild(self, new_lchild: 'Node | None') -> None:
-        self.__lchild = new_lchild
+    def __lt__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.key < other
+        return self.key < other.key
 
-    @property
-    def rchild(self) -> 'Node | None':
-        return self.__rchild
-
-    @rchild.setter
-    def rchild(self, new_rchild: 'Node | None') -> None:
-        self.__rchild = new_rchild
-
-    @property
-    def isroot(self) -> bool:
-        return self.__parent is None
-
-    @property
-    def isleaf(self) -> bool:
-        return self.__lchild is None and self.__rchild is None
-
-    @property
-    def height(self) -> int:
-        h: int = 1
-        parent: 'Node | None' = self.parent
-        while parent is not None:
-            parent = parent.parent
-            h += 1
-        return h
+    def __le__(self, other) -> bool:
+        if isinstance(other, int):
+            return self.key <= other
+        return self.key <= other.key
 
 
 if __name__ == '__main__':
--- tests/test_node.py
+++ tests/test_node.py
@@ -9,36 +9,23 @@ class TestNode(unittest.TestCase):
     def tearDown(self) -> None:
         del self.n
 
-    def test_isroot(self) -> None:
-        self.assertTrue(self.n.isroot)
-        self.n.parent = Node(2)
-        self.assertFalse(self.n.isroot)
-
-    def test_isleaf(self) -> None:
-        self.assertTrue(self.n.isleaf)
-        self.n.lchild = Node(2)
-        self.assertFalse(self.n.isleaf)
-
-    def test_height(self) -> None:
-        self.assertEqual(self.n.height, 1)
-        self.n.lchild = Node(3)
-        self.n.lchild.parent = self.n
-        self.assertEqual(self.n.lchild.height, 2)
-        self.n.lchild.lchild = Node(2)
-        self.n.lchild.lchild.parent = self.n.lchild
-        self.assertEqual(self.n.lchild.lchild.height, 3)
-        self.n.lchild.rchild = Node(4)
-        self.n.lchild.rchild.parent = self.n.lchild
-        self.assertEqual(self.n.lchild.rchild.height, 3)
-        self.n.rchild = Node(6)
-        self.n.rchild.parent = self.n
-        self.assertEqual(self.n.rchild.height, 2)
-        self.n.rchild.rchild = Node(8)
-        self.n.rchild.rchild.parent = self.n.rchild
-        self.assertEqual(self.n.rchild.rchild.height, 3)
-        self.n.rchild.lchild = Node(7)
-        self.n.rchild.lchild.parent = self.n.rchild
-        self.assertEqual(self.n.rchild.lchild.height, 3)
+    def test_comparsions(self) -> None:
+        self.assertEqual(self.n, Node(5))
+        self.assertEqual(self.n, 5)
+        self.assertGreater(self.n, Node(4))
+        self.assertGreater(self.n, 4)
+        self.assertGreaterEqual(self.n, Node(5))
+        self.assertGreaterEqual(self.n, 5)
+        self.assertGreaterEqual(self.n, Node(4))
+        self.assertGreaterEqual(self.n, 4)
+        self.assertLess(self.n, Node(6))
+        self.assertLess(self.n, 6)
+        self.assertLessEqual(self.n, Node(5))
+        self.assertLessEqual(self.n, 5)
+        self.assertLessEqual(self.n, Node(6))
+        self.assertLessEqual(self.n, 6)
+        self.assertNotEqual(self.n, Node(4))
+        self.assertNotEqual(self.n, 4)
 
 
 if __name__ == '__main__':
